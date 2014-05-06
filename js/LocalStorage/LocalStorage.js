5b8c5769ca429d846fb43d7f8f45f16e25deaf74
Jakub Krajč <Jakub Kraj>
2014-05-06 18:28:23 +0200

    js files to separate directories

---
+++ js/LocalStorage/LocalStorage.js
@@ -0,0 +1,36 @@
+/*	
+	----------------------------------
+	Local storage abstract object
+	----------------------------------
+
+	Storage technology Independent 
+*/
+
+function LocalStorage(DSO){
+	this.dso = DSO; //Data Sync Object
+
+	this.ls = new LocalStorageIndexedDb();
+
+	var self = this;
+
+	this.set = function(dataObj, okcallback, kocallback){
+		self.dso.addLocalChange(dataObj);
+		self.setQuietly(dataObj, okcallback, kocallback);
+	}
+
+	this.setQuietly = function(dataObj, okcallback, kocallback){
+		self.ls.set(dataObj, okcallback, kocallback);
+	}
+
+	this.get = function(dataObj, okcallback, kocallback){
+		self.ls.get(dataObj, okcallback, kocallback);
+	}
+
+	this.getAll = function(dataObj, okcallback, kocallback){
+		self.ls.getAll(dataObj, okcallback, kocallback);
+	}
+
+	this.remove = function(dataObj, okcallback, kocallback){
+		self.ls.remove(dataObj, okcallback, kocallback);
+	}
+}
